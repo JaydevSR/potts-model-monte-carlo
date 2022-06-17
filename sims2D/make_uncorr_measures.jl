10d87f2d088a49aa77d5316a76cc3d01efe41a15
Jaydev Singh Rao <jaydevsr7701@gmail.com>
2022-06-17 11:20:55 +0530

    some new measurements

--- sims2D/make_uncorr_measures.jl
+++ sims2D/make_uncorr_measures.jl
@@ -1,7 +1,6 @@
 include("../src/pottsmc.jl")
 
-Lvals = [40]
-# Lvals = [16, 24, 32, 40, 48]
+Lvals = [16, 24, 32, 40, 48]
 # temps = [0.50, 0.70, 0.90, 0.94, 0.98, 1.02, 1.06, 1.10, 1.30, 1.50]
 # τvals = [   2,    3,    4,    5,   13,   23,   14,   14,   40,   61]
 # temps = [0.984, 0.990, 0.996, 1.000, 1.004, 1.01, 1.016]
@@ -13,7 +12,7 @@ temps = [1.012, 1.014, 1.018, 1.022, 1.026, 1.03]
 q=3
 d=2
 nconfigs=5000
-eqsteps=10000
+eqsteps=5000
 store_at="data/magdata/"
 
-potts_getmagdata_to_txt(Lvals, temps, q, d, nconfigs, eqsteps, τvals; store_at=store_at, ntau=4, mode="w")
+potts_getmagdata_to_txt(Lvals, temps, q, d, nconfigs, eqsteps, τvals; store_at=store_at, ntau=3, mode="w")
