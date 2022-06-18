16616870a2b8cf0262dec44b241ecba556f62cd3
JaydevSR <jaydevsr7701@gmail.com>
2022-06-18 17:55:52 +0530

    finite size scaling results

--- sims2D/succeptibility_fss.jl
+++ sims2D/succeptibility_fss.jl
@@ -3,17 +3,29 @@ include("../src/pottsmc.jl")
 data = readdlm("data/max_arg_suzz.txt", ',', Float64)
 
 Lvals = data[:, 1]
+L_ν = Lvals.^-(1/ν)
 T_star = data[:, 2]
-ν = 2
+ν = 5/6
 
 f = Figure()
-ax = Axis(f[1, 1], xlabel = "1/[log(L)]^2", ylabel = "T_c(L)",
+ax = Axis(f[1, 1], xlabel = "L^-1/ν", ylabel = "T_c(L)",
 title = "PottsModel2D: Scaling of critical point using succeptibility")
-# xlims!(0, 0.4)
-xlims!(0, 0.01)
+xlims!(-0.0001, 0.05)
 ylims!(0.98, 1.04)
 
+# linear fit
+data_all = DataFrame(X=L_ν, Y=T_star)
+ols = lm(@formula(Y~X), data_all)
+c_reg, m_reg = coef(ols) 
+x_reg = append!([0.0], L_ν, [0.05])
+y_reg = m_reg .* x_reg .+ c_reg
+
+
 # scatter!(1 ./ (log.(Lvals).^2), T_star)
-scatter!(Lvals.^-(1/ν), T_star)
+lines!(x_reg, y_reg, linestyle=:dot, color=:black, label="linear fit")
+scatter!(L_ν, T_star, label="datapoints")
+scatter!([0], [c_reg], color=:red, marker=:xcross, label="intercept=$(round(c_reg, digits=3))", markersize=16)
+
+axislegend(ax, position=:rb)
 
 display(f)
