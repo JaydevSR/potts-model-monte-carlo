68bcd9283cfcab4f654630381bdd8bb0a29e382e
JaydevSR <jaydevsr7701@gmail.com>
2022-07-14 11:18:01 +0530

    fix new definition of magnetization

--- src/observables.jl
+++ src/observables.jl
@@ -22,18 +22,26 @@ end
 Calculate the total magnetization for q-state potts model.
 """
 
-function magnetization(model::AbstractPottsModel)
+function magnetization(model::AbstractPottsModel; max_def=true)
     counts = zeros(Int64, model.q)
     for site in CartesianIndices(model.lattice)
         counts[model.lattice[site] + 1] += 1
     end
-    return Float64((model.q // (model.q - 1)) * maximum(counts .- model.L^model.d // model.q))
+    if max_def
+        return Float64((model.q // (model.q - 1)) * maximum(counts .- model.L^model.d // model.q))
+    else
+        return sum(counts[p+1] * cos(2*pi*p/model.q) for p=0:model.q-1)
+    end
 end
 
-function magnetization(lattice::AbstractArray, L::Int, q::Int, d::Int)
+function magnetization(lattice::AbstractArray, L::Int, q::Int, d::Int; max_def=true)
     counts = zeros(Int64, q)
     for site in CartesianIndices(lattice)
         counts[lattice[site] + 1] += 1
     end
-    return Float64((q // (q - 1)) * maximum(counts .- L^d // q))
+    if max_def
+        return Float64((q // (q - 1)) * maximum(counts .- L^d // q))
+    else
+        return (2/3)*(sum(counts[p+1] * cos(2*pi*p/q) for p=0:q-1) - (L^d/2))
+    end
 end
