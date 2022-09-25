e47281d73538370c4ebb8e09b08bc1d24fcfbdfb
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-09-25 15:16:56 +0530

    fit exponents

--- sims2D/two_point_correlations/two_point_correlations.jl
+++ sims2D/two_point_correlations/two_point_correlations.jl
@@ -1,5 +1,6 @@
 include("../../src/pottsmc.jl")
 using CairoMakie
+using StatsKit
 
 function spin_prod(s1, s2; q=3)
     theta1 = 2π/q * s1
@@ -20,13 +21,13 @@ for i=1:eqsteps  # equilibration
 end
 
 ss_corr = ss_correlation_fn(potts.lattice, lattice_sizes[end])
-for i in 1:3000
-    for j in 1:20
+for i in 1:10_000
+    for j in 1:12
         wolff_cluster_update!(potts, T_c, fix_vacuum=false, stack=stack, cluster=cluster)
     end
     ss_corr .+= ss_correlation_fn(potts.lattice, lattice_sizes[end])
 end
-ss_corr ./= 3000
+ss_corr ./= 10_000
 ss_corr ./= maximum(ss_corr)
 
 ll = length(ss_corr)
@@ -44,3 +45,22 @@ scatterlines!(ax1, 0:ll-1, ss_corr)
 scatterlines!(ax2, 0:lh, ss_corr[1:lh+1])
 save("plots/2DModel/correlations/two_point_correlations_complete.png", f1)
 save("plots/2DModel/correlations/two_point_correlations_half.png", f2)
+
+# EXpoenents
+
+# linear fit
+XX = log.(1:5)
+YY = log.(ss_corr[2:6])
+data_all = DataFrame(X=XX, Y=YY)
+ols = lm(@formula(Y~X), data_all)
+c_reg, m_reg = coef(ols)
+y_reg = m_reg .* XX .+ c_reg
+
+f3 = Figure();
+ax3 = Axis(f3[1, 1], xlabel="log(r)", ylabel="log(C(r))",
+            title="Two point correlation function (periodic boundary, $L×$L)");
+scatterlines!(ax3, XX, YY, label="log(C(r))")
+scatterlines!(ax3, XX, y_reg, color=:red, 
+                label="linear fit: slope=$(round(m_reg, digits=4)), intercept=$(round(c_reg, digits=4))")
+axislegend(ax3, position=:rt)
+save("plots/2DModel/correlations/two_point_correlations_fit.png", f3)
