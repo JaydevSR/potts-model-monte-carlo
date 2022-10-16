6a1f504498c6ee032419232187d7da9c1950874d
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-10-16 19:10:24 +0530

    edit script

--- sims2D/others/thread_benchmark_script.sh
+++ sims2D/others/thread_benchmark_script.sh
@@ -4,9 +4,8 @@
 NPROC=$(nproc)
 for i in $(seq 1 $NPROC)
 do
-    echo "Running with $i threads" >> thread_benchmark.time
-    { time julia.exe --threads=$i thread_benchmark.jl 2> thread_benchmark.out } 2> thread_benchmark.time
-    echo "" >> thread_benchmark.time
-    # rm thread_benchmark.out
+    echo "Running with $i threads"
+    time julia --threads=$i thread_benchmark.jl
+    echo ""
     rm -rf ./dump
 done
