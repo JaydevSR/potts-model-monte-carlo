b98aee5ab0dd70848c78a440cfe2ce4b5fe379b4
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-10-16 18:31:04 +0530

    redirtection in thread bech script

--- sims2D/others/thread_benchmark_script.sh
+++ sims2D/others/thread_benchmark_script.sh
@@ -4,8 +4,9 @@
 NPROC=$(nproc)
 for i in $(seq 1 $NPROC)
 do
-    echo "Running with $i threads"
-    time julia.exe --threads=$i thread_benchmark.jl
+    echo "Running with $i threads" >> thread_benchmark.time
+    { time julia.exe --threads=$i thread_benchmark.jl 2> thread_benchmark.out } 2> thread_benchmark.time
+    echo "" >> thread_benchmark.time
+    # rm thread_benchmark.out
+    rm -rf ./dump
 done
-
-rm -rf dump
