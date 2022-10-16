b410a42c7edcd728d1cc286aff7b349465df2cc9
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-10-16 17:24:35 +0530

    two point correlations reorganize

---
+++ sims2D/two_point_correlations/two_point_corr_plots.jl
@@ -0,0 +1,45 @@
+using CairoMakie
+using DelimitedFiles
+
+lattice_size = 64
+ss_corr = readdlm("data/ss_correlation_fn_size_64.txt", ',', Float64)[:, 1]
+
+ll = length(ss_corr)
+lh = ll ÷ 2
+
+ss_corr_half = ss_corr[1:lh] + ss_corr[end:-1:lh+1] ./ 2
+
+f1, f2 = Figure(), Figure();
+ax1 = Axis(f1[1, 1], xlabel="r", ylabel="C(r)",
+            title="Two point correlation function (periodic boundary, $lattice_size×$lattice_size)",
+            xticks=0:2:lattice_size, yticks=0:0.02:1);
+ax2 = Axis(f2[1, 1], xlabel="r", ylabel="C(r)",
+            title="Two point correlation function (periodic boundary, $lattice_size×$lattice_size)",
+            xticks=0:2:ll, yticks=0:0.02:1);
+
+scatterlines!(ax1, 0:ll-1, ss_corr)
+scatterlines!(ax2, 0:lh, ss_corr[1:lh+1])
+display(f1)
+display(f2)
+save("plots/2DModel/correlations/two_point_correlations_complete.png", f1)
+save("plots/2DModel/correlations/two_point_correlations_half.png", f2)
+
+# EXpoenents
+
+# linear fit
+XX = log.(1:lh-1)
+YY = log.(ss_corr[2:lh])
+data_all = DataFrame(X=XX, Y=YY)
+ols = lm(@formula(Y~X), data_all)
+c_reg, m_reg = coef(ols)
+y_reg = m_reg .* XX .+ c_reg
+
+f3 = Figure();
+ax3 = Axis(f3[1, 1], xlabel="log(r)", ylabel="log(C(r))",
+            title="Two point correlation function (periodic boundary, $lattice_size×$lattice_size)");
+scatterlines!(ax3, XX, YY, label="log(C(r))")
+scatterlines!(ax3, XX, y_reg, color=:red, 
+                label="linear fit: slope=$(round(m_reg, digits=4)), intercept=$(round(c_reg, digits=4))")
+axislegend(ax3, position=:rt)
+display(f3)
+save("plots/2DModel/correlations/two_point_correlations_fit.png", f3)
--- sims2D/two_point_correlations/two_point_correlations.jl
+++ sims2D/two_point_correlations/two_point_correlations.jl
@@ -1,6 +1,6 @@
 include("../../src/pottsmc.jl")
-using CairoMakie
 using StatsKit
+using DelimitedFiles
 
 function spin_prod(s1, s2; q=3)
     theta1 = 2π/q * s1
@@ -8,11 +8,15 @@ function spin_prod(s1, s2; q=3)
     return real(exp(im*(theta1 + theta2)))
 end
 
-lattice_sizes = [32, 48, 64]
-eqsteps = 10000
+# lattice_size = 64
+# eqsteps = 10000
+# n_steps = 50000
+lattice_size = 128
+eqsteps = 2000
+n_steps = 10000
 
 T_c = 1/log1p(sqrt(3))
-potts = PottsModel2D(lattice_sizes[end], 3, :cold)
+potts = PottsModel2D(lattice_size, 3, :cold)
 stack=LazyStack(Int)
 cluster=falses(size(potts.lattice))
 
@@ -20,47 +24,17 @@ for i=1:eqsteps  # equilibration
     wolff_cluster_update!(potts, T_c, fix_vacuum=false, stack=stack, cluster=cluster)
 end
 
-ss_corr = ss_correlation_fn(potts.lattice, lattice_sizes[end])
-for i in 1:10_000
+ss_corr = ss_correlation_fn(potts.lattice, lattice_size)
+for i in 1:n_steps
     for j in 1:12
         wolff_cluster_update!(potts, T_c, fix_vacuum=false, stack=stack, cluster=cluster)
     end
-    ss_corr .+= ss_correlation_fn(potts.lattice, lattice_sizes[end])
+    ss_corr .+= ss_correlation_fn(potts.lattice, lattice_size)
 end
-ss_corr ./= 10_000
+ss_corr ./= n_steps
 ss_corr ./= maximum(ss_corr)
 
-ll = length(ss_corr)
-lh = ll ÷ 2
-
-f1, f2 = Figure(), Figure();
-ax1 = Axis(f1[1, 1], xlabel="r", ylabel="C(r)",
-            title="Two point correlation function (periodic boundary, $L×$L)",
-            xticks=0:2:L, yticks=0:0.02:1);
-ax2 = Axis(f2[1, 1], xlabel="r", ylabel="C(r)",
-            title="Two point correlation function (periodic boundary, $L×$L)",
-            xticks=0:2:ll, yticks=0:0.02:1);
-
-scatterlines!(ax1, 0:ll-1, ss_corr)
-scatterlines!(ax2, 0:lh, ss_corr[1:lh+1])
-save("plots/2DModel/correlations/two_point_correlations_complete.png", f1)
-save("plots/2DModel/correlations/two_point_correlations_half.png", f2)
-
-# EXpoenents
-
-# linear fit
-XX = log.(1:5)
-YY = log.(ss_corr[2:6])
-data_all = DataFrame(X=XX, Y=YY)
-ols = lm(@formula(Y~X), data_all)
-c_reg, m_reg = coef(ols)
-y_reg = m_reg .* XX .+ c_reg
-
-f3 = Figure();
-ax3 = Axis(f3[1, 1], xlabel="log(r)", ylabel="log(C(r))",
-            title="Two point correlation function (periodic boundary, $L×$L)");
-scatterlines!(ax3, XX, YY, label="log(C(r))")
-scatterlines!(ax3, XX, y_reg, color=:red, 
-                label="linear fit: slope=$(round(m_reg, digits=4)), intercept=$(round(c_reg, digits=4))")
-axislegend(ax3, position=:rt)
-save("plots/2DModel/correlations/two_point_correlations_fit.png", f3)
+# Write ss_corr function
+open("data/ss_correlation_fn_size_$(lattice_size).txt", "w") do f
+    writedlm(f, ss_corr, ',')
+end
