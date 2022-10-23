f3be62695db968fc01e98e0421fcb3c3adf60841
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-10-23 12:32:48 +0530

    modify cumulant ratios

--- sims2D/magnetisation_cumulants/cumulant_ratios.jl
+++ sims2D/magnetisation_cumulants/cumulant_ratios.jl
@@ -3,15 +3,16 @@ using CairoMakie
 
 suzz_kth(m_arr, T, nsites, k) = (1/T) * (nsites) * cumulant(m_arr, k)
 
-lattice_sizes=[16, 32, 48, 64]
+lattice_sizes = [16, 32, 48, 64, 80]
 temps = [0.980, 0.984, 0.988, 0.992, 0.996, 1.000, 1.004,
-     1.008, 1.012, 1.016, 1.020, 1.024, 1.028, 1.032, 1.036, 1.040]
+    1.008, 1.012, 1.016, 1.020, 1.024, 1.028, 1.032, 1.036, 1.040]
 max_order = 6
 errors = zeros(Float64, (length(lattice_sizes), max_order, length(temps)))
 suzzs = zeros(Float64, (length(lattice_sizes), max_order, length(temps)))
+
 for stepL in eachindex(lattice_sizes)
     L = lattice_sizes[stepL]
-    for idx in eachindex(temps)
+    Threads.@threads for idx in eachindex(temps)
         T = temps[idx]
         mags = readdlm(joinpath("data", "2DModel", "Size$(L)", "mags", "potts_mags_temp$(T)_L$(L).txt"), ',', Float64)
         mags ./= L^2
@@ -25,22 +26,70 @@ for stepL in eachindex(lattice_sizes)
     end
 end
 
+# Plot the data
+for order in 1:max_order
+    let f = Figure(resolution=(800, 600))
+        ax = Axis(f[1, 1], xlabel="T", ylabel="Order $(order) value", title="Order $(order) derivative of ln(Z) with respect to h as h → 0")
+        for stepL in eachindex(lattice_sizes)
+            L = lattice_sizes[stepL]
+            errorbars!(ax, temps, suzzs[stepL, order, :], errors[stepL, order, :], whiskerwidth = 12)
+            scatterlines!(ax, temps, suzzs[stepL, order, :], markersize = 12, label="size=$(L)×$(L)", linestyle = :dashdot,
+            marker=:diamond, linewidth=3)
+        end
+        axislegend(ax, position=:rt)
+        save("plots/2DModel/cumulants/order_$(order).svg", f)
+    end
+end
+
+order_colors = [:red, :blue, :dodgerblue, :dodgerblue, :orange, :orange]
+# Plot Ratios
+for stepL in eachindex(lattice_sizes)
+    L = lattice_sizes[stepL]
+    suzzs_L = suzzs[stepL, :, :] .± errors[stepL, :, :]
+    f = Figure(resolution=(1000, 600));
+    axes = [Axis(f[1, 1], xlabel="T", ylabel="Ratio", title="Ratio of odd orders for L=$L"),
+        Axis(f[1, 2], xlabel="T", ylabel="Ratio", title="Ratio of even orders for L=$L")]
+
+    for order in 3:2:max_order
+        ratio = suzzs_L[order, :] ./ suzzs_L[order-2, :]
+        ratio_values = Measurements.value.(ratio)
+        ratio_errors = Measurements.uncertainty.(ratio)
+
+        band!(axes[1], temps, ratio_values .- ratio_errors, ratio_values .+ ratio_errors,
+            color = (order_colors[order], 0.25), label="Order $(order) / Order 1")
+        errorbars!(axes[1], temps, ratio_values, ratio_errors, label="Order $(order) / Order 1", whiskerwidth=12)
+        scatterlines!(axes[1], temps, ratio_values, label="Order $(order) / Order 1", color=order_colors[order])
+    end
+
+    for order in 4:2:max_order
+        ratio = suzzs_L[order, :] ./ suzzs_L[2, :]
+        ratio_values = Measurements.value.(ratio)
+        ratio_errors = Measurements.uncertainty.(ratio)
+
+        band!(axes[2], temps, ratio_values .- ratio_errors, ratio_values .+ ratio_errors,
+            color = (order_colors[order], 0.25), label="Order $(order) / Order 2")
+        errorbars!(axes[2], temps, ratio_values, ratio_errors, label="Order $(order) / Order 2", whiskerwidth=12)
+        scatterlines!(axes[2], temps, ratio_values, label="Order $(order) / Order 2", color=order_colors[order])
+    end
+
+    axislegend(axes[1], position=:lt, merge=true)
+    axislegend(axes[2], position=:rt, merge=true)
+
+    # display(f)
+    save(joinpath("plots", "2DModel", "cumulants", "cumulant_ratios_size$(L).svg"), f)
+end
+
+# save values
 for stepL in eachindex(lattice_sizes)
     L = lattice_sizes[stepL]
     suzzs_L = suzzs[stepL, :, :]
-    T_star = temps[argmax(suzzs_L[2, :])]
-    f = Figure(resolution=(1000, 600));
-    axes = [Axis(f[1, 1], xlabel="T", ylabel="Ratio", title="Ratio: Order 3 by Order 1 for L=$L"),
-        Axis(f[2, 1], xlabel="T", ylabel="Ratio", title="Ratio: Order 4 by Order 2 for L=$L"),
-        Axis(f[3, 1], xlabel="T", ylabel="Ratio", title="Ratio: Order 5 by Order 3 for L=$L"),
-        ]
-
-    for idx in eachindex(axes)
-        ax = axes[idx]
-        scatterlines!(ax, temps, suzzs_L[idx+3, :] ./ suzzs_L[idx+1, :])
-        vlines!(ax, T_star, color=:blue, linestyle=:dash)
-        hlines!(ax, 0, color=:blue, linestyle=:dash)
+    errors_L = errors[stepL, :, :]
+    mkpath(joinpath("data", "2DModel", "Size$(L)", "cumulants"))
+    open(joinpath("data", "2DModel", "Size$(L)", "cumulants", "cumulant_values.txt"), "w") do io
+        writedlm(io, suzzs_L, ',')
     end
 
-    save(joinpath("plots", "2DModel", "cumulants", "cumulant_ratios_size$(L).svg"), f)
+    open(joinpath("data", "2DModel", "Size$(L)", "cumulants", "cumulant_errors.txt"), "w") do io
+        writedlm(io, errors_L, ',')
+    end
 end
