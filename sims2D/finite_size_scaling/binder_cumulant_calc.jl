ec4aa0aaa32db63fa0e7bb4bb6d1bfb46245516c
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-10-23 23:52:42 +0530

    add to binders cumulant

--- sims2D/finite_size_scaling/binder_cumulant_calc.jl
+++ sims2D/finite_size_scaling/binder_cumulant_calc.jl
@@ -3,8 +3,9 @@ using CairoMakie
 
 lattice_sizes = [32, 48, 64, 80]
 cols = Dict([(32, :blue), (48, :red), (64, :green), (80, :purple)])
-temps = [0.900, 0.920, 0.940, 0.960, 0.980, 0.984, 0.988, 0.992, 0.996, 1.000, 1.004,
-         1.008, 1.012, 1.016, 1.020, 1.024, 1.028, 1.032, 1.036, 1.040, 1.060, 1.080, 1.100]
+temps = [0.900, 0.920, 0.940, 0.960, 0.980, 0.982, 0.984, 0.986, 0.988, 0.990, 0.992, 0.994, 0.996, 0.998,
+        1.000, 1.002, 1.004, 1.006, 1.008, 1.010, 1.012, 1.014, 1.016, 1.018, 1.020, 1.022, 1.024, 1.026,
+        1.028, 1.030, 1.032, 1.034, 1.036, 1.038, 1.040, 1.060, 1.080, 1.100]
 
 mags_def = 1
 
@@ -13,7 +14,7 @@ err_binder = zeros(Float64, (length(lattice_sizes), length(temps)))
 
 for Lidx in eachindex(lattice_sizes)
     L = lattice_sizes[Lidx]
-    for tidx in eachindex(temps)
+    Threads.@threads for tidx in eachindex(temps)
         T = temps[tidx]
         mags = readdlm(joinpath("data", "2DModel", "Size$(L)", "mags", "potts_mags_temp$(T)_L$(L).txt"), ',', Float64)
         mags ./= L^2
@@ -24,7 +25,7 @@ for Lidx in eachindex(lattice_sizes)
 end
 
 # plots
-trange = 7:10
+trange = 10:14
 f = Figure(resolution=(800, 600));
 
 ax1 = Axis(f[1, 1], xlabel = "T", ylabel = "U",
