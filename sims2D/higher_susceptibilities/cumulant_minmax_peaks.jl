f23ddda0357d86e7c44c6893ceee3a033ac17e6f
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2023-01-03 18:21:37 +0530

    rename and magnetisation distribution plots

--- sims2D/higher_susceptibilities/cumulant_minmax_peaks.jl
+++ sims2D/higher_susceptibilities/cumulant_minmax_peaks.jl
@@ -2,11 +2,11 @@ include("../../src/pottsmc.jl")
 using CairoMakie
 using LsqFit
 
-Tc_inf = 0.99406782
+Tc_inf = 0.99628906
 lattice_sizes = [48, 56, 64, 72, 80, 96, 128]
 cols = Dict([(32, :blue), (48, :red), (56, :pink), (64, :green), (72, :orange), (80, :purple), (96, :cyan), (128, :deepskyblue)])
 temps = [0.982, 0.984, 0.986, 0.988, 0.990, 0.992, 0.994, 0.996, 0.998,
-         1.000, 1.002, 1.004, 1.006, 1.008, 1.010, 1.012, 1.014, 1.016]
+         1.000, 1.002, 1.004, 1.006, 1.008, 1.010, 1.012, 1.014, 1.016, 1.018, 1.020]
 
 mags_def = 1
 suzz_order = 3
@@ -19,7 +19,7 @@ err_suzz = zeros(Float64, (length(lattice_sizes), length(temps)))
 
 parabola(x, p) = p[1] .+ p[2]*x .+ p[3]*x.^2
 
-parabola_extrema(p) = -p[2]/(2*p[3])
+parabola_extrema(p) = -p[2] / (2 * p[3])
 p0 = [0.0, 0.0, 1.0]
 
 println("Calculating susceptibilities ...")
@@ -37,8 +37,8 @@ for Lidx in eachindex(lattice_sizes)
     end
 end
 
-min_locations = zeros(Float64, length(lattice_sizes))
-max_locations = zeros(Float64, length(lattice_sizes))
+min_locations = fill(0.0 ± 0.0, length(lattice_sizes))
+max_locations = fill(0.0 ± 0.0, length(lattice_sizes))
 
 println("Plotting ...")
 fs = Figure();
@@ -47,17 +47,17 @@ for Lidx=eachindex(lattice_sizes)
     L = lattice_sizes[Lidx]
     max_peak = findmax(suzz[Lidx, :])
     min_peak = findmin(suzz[Lidx, :])
-    min_range = (min_peak[2]-1):(min_peak[2]+1)
-    max_range = (max_peak[2]-1):(max_peak[2]+1)
+    min_range = (min_peak[2]-2):(min_peak[2]+2)
+    max_range = (max_peak[2]-2):(max_peak[2]+2)
 
     fit_min = curve_fit(parabola, temps[min_range], suzz[Lidx, min_range], p0)
     fit_max = curve_fit(parabola, temps[max_range], suzz[Lidx, max_range], p0)
 
-    min_locations[Lidx] = parabola_extrema(fit_min.param)
-    max_locations[Lidx] = parabola_extrema(fit_max.param)
+    min_locations[Lidx] = parabola_extrema(fit_min.param .± stderror(fit_min))
+    max_locations[Lidx] = parabola_extrema(fit_max.param .± stderror(fit_max))
 
-    lines!(axs, temps[min_range[1]]:0.0001:temps[min_range[end]], parabola(temps[min_range[1]]:0.0001:temps[min_range[end]], fit_min.param), color=:grey69, label="L = $L")
-    lines!(axs, temps[max_range[1]]:0.0001:temps[max_range[end]], parabola(temps[max_range[1]]:0.0001:temps[max_range[end]], fit_max.param), color=:grey69, label="L = $L")
+    lines!(axs, temps[min_range[1]]:0.0001:temps[min_range[end]], parabola(temps[min_range[1]]:0.0001:temps[min_range[end]], fit_min.param), color=:grey7, label="L = $L")
+    lines!(axs, temps[max_range[1]]:0.0001:temps[max_range[end]], parabola(temps[max_range[1]]:0.0001:temps[max_range[end]], fit_max.param), color=:grey7, label="L = $L")
     errorbars!(axs, temps, suzz[Lidx, :], err_suzz[Lidx, :], color=cols[L], label="L = $L", whiskerwidth=12)
     scatterlines!(axs, temps, suzz[Lidx, :], color=cols[L], label="L = $L")
 end
@@ -69,10 +69,12 @@ display(fs)
 println("Plotting ...")
 f = Figure();
 ax = Axis(f[1, 1], xlabel="1/L", ylabel="T - Tc_inf")
-scatterlines!(ax, inv.(lattice_sizes), max_locations .- Tc_inf, color=:red, label="Maxima")
-scatterlines!(ax, inv.(lattice_sizes), min_locations .- Tc_inf, color=:blue, label="Minima")
+scatterlines!(ax, inv.(lattice_sizes), Measurements.value.(max_locations) .- Tc_inf, color=:red, label="Maxima")
+scatterlines!(ax, inv.(lattice_sizes), Measurements.value.(min_locations) .- Tc_inf, color=:blue, label="Minima")
 axislegend(ax, merge=true)
 display(f)
 
 amax, bmax = linear_least_squares(inv.(lattice_sizes), max_locations .- Tc_inf)
 amin, bmin = linear_least_squares(inv.(lattice_sizes), min_locations .- Tc_inf)
+
+# TODO: THE ERROR ESTIMATES SUCK ASS
---
+++ sims2D/others/magnetisation_distribution.jl
@@ -0,0 +1,30 @@
+include("../../src/pottsmc.jl")
+using CairoMakie
+
+temperature = 1.5
+lattice_size = 32
+eqsteps = 1000
+msteps = 1_00_000
+mag_definition = :vac
+
+potts = PottsModel2D(lattice_size, 3, :cold)
+
+stack=LazyStack(Int)
+cluster=falses(size(potts.lattice))
+
+mags = zeros(Float64, msteps)
+
+for i=1:eqsteps  # equilibration
+    wolff_cluster_update!(potts, temperature; fix_vacuum=true, stack=stack, cluster=cluster)
+end
+
+for j in 1:msteps
+    wolff_cluster_update!(potts, temperature; fix_vacuum=true, stack=stack, cluster=cluster)
+    mags[j] = magnetization(potts, use_definition=mag_definition)
+end
+
+f = Figure();
+ax = Axis(f[1,1], xlabel="Magnetization", ylabel="density", title="Magnetization distribution: T = $temperature, L = $lattice_size")
+density!(mags, npoints=500, color=("blue", 0.5), strokewidth = 1.5,
+strokecolor = :grey20)
+display(f)
