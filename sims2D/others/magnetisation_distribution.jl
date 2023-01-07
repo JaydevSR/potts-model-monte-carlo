8d61b1d020317ab3f1bb48c5d6e8e243de49cdce
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2023-01-07 11:32:10 +0530

    fix acceptance for magnetic field

--- sims2D/others/magnetisation_distribution.jl
+++ sims2D/others/magnetisation_distribution.jl
@@ -2,10 +2,12 @@ include("../../src/pottsmc.jl")
 using CairoMakie
 
 temperature = 1.5
+external_field = 0.05
 lattice_size = 32
 eqsteps = 1000
 msteps = 1_00_000
 mag_definition = :vac
+accepted = 0
 
 potts = PottsModel2D(lattice_size, 3, :cold)
 
@@ -15,13 +17,15 @@ cluster=falses(size(potts.lattice))
 mags = zeros(Float64, msteps)
 
 for i=1:eqsteps  # equilibration
-    wolff_cluster_update!(potts, temperature; fix_vacuum=true, stack=stack, cluster=cluster)
+    wolff_cluster_update!(potts, temperature, external_field; fix_vacuum=true, stack=stack, cluster=cluster)
 end
 
 for j in 1:msteps
-    wolff_cluster_update!(potts, temperature; fix_vacuum=true, stack=stack, cluster=cluster)
+    accepted += wolff_cluster_update!(potts, temperature, external_field; fix_vacuum=true, stack=stack, cluster=cluster)
     mags[j] = magnetization(potts, use_definition=mag_definition)
 end
+
+@info "Acceptance ratio: $(accepted/msteps)"
 
 f = Figure();
 ax = Axis(f[1,1], xlabel="Magnetization", ylabel="density", title="Magnetization distribution: T = $temperature, L = $lattice_size")
--- src/mcalgos.jl
+++ src/mcalgos.jl
@@ -49,7 +49,8 @@ end
 # WOLFF CLUSTER ALGORITHM
 function wolff_cluster_update!(
                     model::PottsModel2D,
-                    temp::Float64;
+                    temp::Float64,
+                    h::Float64=0.0;
                     P_add::Float64=-expm1(-1/temp), fix_vacuum::Bool=true,
                     stack::LazyStack{Int}=LazyStack(Int),
                     cluster::BitMatrix=falses(model.L, model.L))
@@ -59,21 +60,20 @@ function wolff_cluster_update!(
         error("Cluster must be of size $(size(model.lattice))")
     end
     cluster .= false
+    cluster_size = 0
     
     seedx, seedy = rand(1:model.L), rand(1:model.L)
     push!(stack, seedx)
     push!(stack, seedy)
     @inbounds sval = model.lattice[seedx, seedy]
-    new_val = mod(rand((sval + 1):(sval + model.q - 1)), model.q)
-    @inbounds cluster[seedx, seedy] = true
 
+    # build the cluster
+    @inbounds cluster[seedx, seedy] = true
+    cluster_size += 1
     while !isempty(stack)
         ky = pop!(stack)
         kx = pop!(stack)
         @inbounds kval = model.lattice[kx, ky]
-        @inbounds model.lattice[kx, ky] = new_val  # set new value
-        @inbounds model.counts[new_val+1] += 1
-        @inbounds model.counts[kval+1] -= 1
         for δ ∈ model.shifts
             nnx, nny = mod1(kx + δ[1], model.L), mod1(ky + δ[2], model.L)
             @inbounds nnval = model.lattice[nnx, nny]
@@ -81,25 +81,36 @@ function wolff_cluster_update!(
                 push!(stack, nnx)
                 push!(stack, nny)
                 @inbounds cluster[nnx, nny] = true
+                cluster_size += 1
             end
         end
     end
 
-    if fix_vacuum
-        Φ = argmax(model.counts) - 1  # current vacuum
-        rotation = ntuple(s -> mod(s-Φ-1, model.q), model.q)
-        map!(s -> rotation[s+1], model.lattice, model.lattice)
-        unrot_counts = copy(model.counts)
-        for i in eachindex(model.counts)
-            model.counts[rotation[i]+1] = unrot_counts[i]
+    new_val = mod(rand((sval + 1):(sval + model.q - 1)), model.q)
+    ΔE_mag = - h * ((new_val == 0) - (sval == 0)) * cluster_size
+    # flip the cluster
+    if ΔE_mag <= 0 || rand() < exp(-ΔE_mag / temp)
+        @inbounds model.lattice[cluster] .= new_val
+        @inbounds model.counts[new_val+1] += cluster_size
+        @inbounds model.counts[sval+1] -= cluster_size
+        if fix_vacuum
+            Φ = argmax(model.counts) - 1  # current vacuum
+            rotation = ntuple(s -> mod(s-Φ-1, model.q), model.q)
+            map!(s -> rotation[s+1], model.lattice, model.lattice)
+            unrot_counts = copy(model.counts)
+            for i in eachindex(model.counts)
+                model.counts[rotation[i]+1] = unrot_counts[i]
+            end
         end
+        return true
     end
-    nothing
+    return false
 end
 
 function wolff_cluster_update!(
                     model::PottsModel3D,
-                    temp::Float64;
+                    temp::Float64,
+                    h::Float64=0.0;
                     P_add::Float64=-expm1(-1/temp), fix_vacuum::Bool=true,
                     stack::LazyStack{Int}=LazyStack(Int),
                     cluster::BitArray{3}=falses(model.L, model.L, model.L)) 
@@ -108,24 +119,24 @@ function wolff_cluster_update!(
     if size(cluster) != size(model.lattice)
         error("Cluster must be of size $(size(model.lattice))")
     end
+
     cluster .= false
-    
+    cluster_size = 0
+
     seedx, seedy, seedz = rand(1:model.L), rand(1:model.L), rand(1:model.L)
     push!(stack, seedx)
     push!(stack, seedy)
     push!(stack, seedz)
-    sval = model.lattice[seedx, seedy, seedz]
-    new_val = mod(rand((sval + 1):(sval + model.q - 1)), model.q)
-    cluster[seedx, seedy, seedz] = true
+    @inbounds sval = model.lattice[seedx, seedy, seedz]
 
+    # build the cluster
+    @inbounds cluster[seedx, seedy, seedz] = true
+    cluster_size += 1
     while !isempty(stack)
         kz = pop!(stack)
         ky = pop!(stack)
         kx = pop!(stack)
         @inbounds kval = model.lattice[kx, ky, kz]
-        @inbounds model.lattice[kx, ky, kz] = new_val  # set new value
-        model.counts[new_val+1] += 1
-        model.counts[kval+1] -= 1
         for δ ∈ model.shifts
             nnx, nny, nnz = mod1(kx + δ[1], model.L), mod1(ky + δ[2], model.L), mod1(kz + δ[3], model.L)
             @inbounds nnval = model.lattice[nnx, nny, nnz]
@@ -134,18 +145,28 @@ function wolff_cluster_update!(
                 push!(stack, nny)
                 push!(stack, nnz)
                 @inbounds cluster[nnx, nny, nnz] = true
+                cluster_size += 1
             end
         end
     end
 
-    if fix_vacuum
-        current_vacuum = argmax(model.counts) - 1
-        rotation = Tuple(mod(s - current_vacuum, model.q) for s=0:model.q-1)
-        map!(s -> rotation[s+1], model.lattice, model.lattice)
-        unrot_counts = copy(model.counts)
-        for i in eachindex(model.counts)
-            model.counts[rotation[i]+1] = unrot_counts[i]
+    new_val = mod(rand((sval + 1):(sval + model.q - 1)), model.q)
+    ΔE_mag = - h * ((new_val == 0) - (sval == 0)) * cluster_size
+    # flip the cluster
+    if ΔE_mag <= 0 || rand() < exp(-ΔE_mag / temp)
+        @inbounds model.lattice[cluster] .= new_val
+        @inbounds model.counts[new_val+1] += cluster_size
+        @inbounds model.counts[sval+1] -= cluster_size
+        if fix_vacuum
+            Φ = argmax(model.counts) - 1  # current vacuum
+            rotation = ntuple(s -> mod(s-Φ-1, model.q), model.q)
+            map!(s -> rotation[s+1], model.lattice, model.lattice)
+            unrot_counts = copy(model.counts)
+            for i in eachindex(model.counts)
+                model.counts[rotation[i]+1] = unrot_counts[i]
+            end
         end
+        return true
     end
-    nothing
+    return false
 end
