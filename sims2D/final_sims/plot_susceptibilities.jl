7d032cf81806fc245bed4cd6e72a00f9bc00cc43
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2023-03-18 16:33:44 +0530

    increase order and renames

--- sims2D/final_sims/plot_susceptibilities.jl
+++ sims2D/final_sims/plot_susceptibilities.jl
@@ -6,16 +6,30 @@ lattice_sizes = [48, 56, 64, 72, 80]
 cols = Dict([(32, :blue), (48, :red), (56, :pink), (64, :green), (72, :orange), (80, :purple), (96, :cyan), (128, :deepskyblue)])
 
 temps = reshape(readdlm(joinpath("data", "2DModel", "susceptibilities", "potts_temps.txt"), ',', Float64), :)
-max_order = 5
+max_order = 6
 
 figures = [Figure(resolution=(800, 600)) for i in 1:max_order]
 axes = [
     Axis(
+        figures[1][1, 1],
+        xlabel=L"T", ylabel=L"m(T)",
+        title=L"m(T) = \frac{1}{L^2} \left[\frac{\partial \ln Z}{\partial h}\right]_{h \rightarrow 0}"
+    ),
+
+    Axis(
+        figures[2][1, 1],
+        xlabel=L"T", ylabel=L"\chi(T)",
+        title=L"\chi(T) = \frac{1}{L^2} \left[\frac{\partial^{2} \ln Z}{\partial h^2}\right]_{h \rightarrow 0}"
+    )
+]
+
+append!(axes, [
+    Axis(
         figures[i][1, 1],
         xlabel=L"T", ylabel=L"\chi_{%$i}(T)",
-        title=L"\chi_{%$i}(T) = \left[\frac{\partial^{%$i} \ln Z}{\partial h^{%$i}}\right]_{h \rightarrow 0}"
-    ) for i in 1:max_order
-]
+        title=L"\chi_{%$i}(T) = \frac{1}{L^2} \left[\frac{\partial^{%$i} \ln Z}{\partial h^{%$i}}\right]_{h \rightarrow 0}"
+    ) for i in 3:max_order
+])
 
 for i in 1:max_order
     xlims!(axes[i], (0.98, 1.022))
@@ -39,6 +53,8 @@ for idx in 2:max_order
     axislegend(axes[idx], position=:lt, merge=true)
 end
 
-for idx in 1:max_order
-    save(joinpath("plots", "2DModel", "final_plots", "potts_suzzs_order$(idx).svg"), figures[idx])
+save(joinpath("plots", "2DModel", "final_plots", "potts_magnetisation.svg"), figures[1])
+save(joinpath("plots", "2DModel", "final_plots", "potts_susceptibility.svg"), figures[2])
+for idx in 3:max_order
+    save(joinpath("plots", "2DModel", "final_plots", "potts_susceptibility_order$(idx).svg"), figures[idx])
 end
